77ceca44450d79bf4116b451c9a4eb496ab5fa65
Tischlinger Alex <32167982+Alexikon@users.noreply.github.com>
2019-12-07 09:00:45 +0100

    testing import of pokemon

---
+++ PokemonImportTesting/PokemonImportTesting/Pokemon.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PokemonImportTesting
+{
+    // Pokemon from https://pokeapi.co/api/v2/pokemon/
+    class PokemonUrl
+    {
+        [JsonPropertyName("next")]
+        public string Next { get; set; }
+
+        [JsonPropertyName("previous")]
+        public string Previous { get; set; }
+
+        [JsonPropertyName("results")]
+        public List<Results> Results { get; set; }
+    }
+
+    // Pokemon results with name and url
+    public class Results
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+    }
+
+    // real pokemon data from https://pokeapi.co/api/v2/pokemon/{id}/
+    public class Pokemon
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("weight")]
+        public int Weight { get; set; }
+
+        [JsonPropertyName("abilities")]
+        public List<Abilities> Abilities { get; set; }
+
+        [JsonPropertyName("sprites")]
+        public Sprites SpritesUrls { get; set; }
+    }
+
+    // real pokemon data abilities
+    public class Abilities
+    {
+        [JsonPropertyName("ability")]
+        public Ability Ability { get; set; }
+    }
+
+    // real pokemon data ability
+    public class Ability
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+    
+    // real pokemon data sprites
+    public class Sprites
+    {
+        [JsonPropertyName("front_default")]
+        public string FrontDefaultUrl { get; set; }
+
+        //public Image FrontDefault { get; set; }
+
+        [JsonPropertyName("back_default")]
+        public string BackSpriteUrl { get; set; }
+
+        //public Image BackDefault { get; set; }
+    }
+}
---
+++ PokemonImportTesting/PokemonImportTesting/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PokemonImportTesting
+{
+    class Program
+    {
+        private static readonly HttpClient HttpClient = new HttpClient();
+        private const string Url = "https://pokeapi.co/api/v2/pokemon/";
+        public static PokemonUrl PokemonUrl { get; set; }
+        public static List<Pokemon> Pokemon { get; set; }
+
+        static async Task Main(string[] args)
+        {
+            Pokemon = new List<Pokemon>();
+
+            var pokemonResponse = await HttpClient.GetAsync(Url);
+            pokemonResponse.EnsureSuccessStatusCode();
+            var responseBody = await pokemonResponse.Content.ReadAsStringAsync();
+            PokemonUrl = JsonSerializer.Deserialize<PokemonUrl>(responseBody);
+
+            foreach(var pok in PokemonUrl.Results)
+            {
+                pokemonResponse = await HttpClient.GetAsync(pok.Url);
+                pokemonResponse.EnsureSuccessStatusCode();
+                responseBody = await pokemonResponse.Content.ReadAsStringAsync();
+                var pokemon = JsonSerializer.Deserialize<Pokemon>(responseBody);
+
+                Console.WriteLine(pokemon.Name + "\t\t" + pokemon.Weight + "\t" + pokemon.Abilities[0].Ability.Name);
+
+                Pokemon.Add(pokemon);
+            }
+        }
+    }
+}
